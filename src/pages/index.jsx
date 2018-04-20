c8d94885fec82fed4d43ea9703873d564101b705
Brady <bsutt123@gmail.com>
2018-04-20 13:26:43 -0400

    create a simpler index view

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+import styles  from './styles/index.module.css';
+
+const Index = () => {
+  return (
+    <div className={styles.container}> IndexPage! </div>
+  )
+}
