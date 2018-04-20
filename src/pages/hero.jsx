4f5e2bce2e48ae75cab54c41f1bff4a33cdef917
Brady <bsutt123@gmail.com>
2018-04-20 13:26:53 -0400

    create the hero image component page

---
+++ src/pages/hero.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import styles from "./styles/hero.module.css";
+
+class Hero extends Component {
+  render() {
+    return (
+      <div className={styles.hero}>
+        <img
+          className={styles.heroImgTop}
+          src="/img/mech_engineer.jpg"
+          alt="engineering"
+        />
+        <img
+          className={styles.heroImg}
+          src="/img/dev-typing.jpg"
+          alt="computer"
+        />
+        <div className={styles.heroTitle}>
+          <h1 className={styles.headingPrimary}>Brady Sutton</h1>
+          <h3 className={styles.headingSecondary}>
+            <span className={styles.fadeIn}> Former Mechanical engineer</span>
+            ...
+            <span className={styles.fadeOut}> turned software developer </span>
+          </h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Hero;
