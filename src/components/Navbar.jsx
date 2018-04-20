1f35c56eef7b5c17ebd7abb561215014beee8fb1
Brady <bsutt123@gmail.com>
2018-04-20 17:39:42 -0400

    slug navbar out for later development

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import github from '../img/github-icon.svg'
+import logo from '../img/logo.svg'
+
+import styles from './styles/navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      I'm the NavBar!
+    </nav>
+  )
+}
+export default Navbar
