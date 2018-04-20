8853186b198c147a0ad68c9fb58f163529b76ed1
Brady <bsutt123@gmail.com>
2018-04-20 17:38:38 -0400

    update the index page and layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import Link from 'gatsby-link';
+import React, { Component } from "react";
+import styles from "./styles/hero.module.css";
 
-import styles  from './styles/index.module.css';
-
-const Index = () => {
-  return (
-    <div className={styles.container}> IndexPage! </div>
-  )
+class Hero extends Component {
+  render() {
+    return (
+      <div className={styles.hero}>
+      <img
+      className={styles.heroImgTop}
+      src="/img/mech_engineer.jpg"
+      alt="engineering"
+      />
+      <img
+      className={styles.heroImg}
+      src="/img/dev-typing.jpg"
+      alt="computer"
+      />
+      <div className={styles.heroTitle}>
+        <h1 className={styles.headingPrimary}>Brady Sutton</h1>
+        <h3 className={styles.headingSecondary}>
+          <span className={styles.fadeIn}> Former Mechanical engineer</span>
+          ...
+          <span className={styles.fadeOut}> turned software developer </span>
+        </h3>
+      </div>
+      </div>
+    );
+  }
 }
 
-export default Index
+export default Hero;
